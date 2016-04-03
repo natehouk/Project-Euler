617d7c07803b22246ab1767e122d565eca5f7a00
Nate Houk <houk@gatech.edu>
2016-04-03 23:42:28 +0200

    Problem #5 Solved

---
+++ problem-5.py
@@ -0,0 +1,9 @@
+answer = 20
+while True:
+    for i in range(1, 21):
+        if (answer % i != 0):
+            break
+        elif (i == 20):
+            print answer
+            exit()
+    answer += 20
