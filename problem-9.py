afb0d8575e04577dd91543dd13c050e3f0b38e6a
Nate Houk <nate.houk@gmail.com>
2020-12-27 13:49:13 +0100

    Problem #9 Solved

---
+++ problem-9.py
@@ -0,0 +1,7 @@
+for a in range(1, 1000):
+    for b in range(a, 1000):
+        for c in range(b, 1000):
+            if a**2 + b**2 == c**2:
+                if a + b + c == 1000:
+                    print(a*b*c)
+                    exit()
